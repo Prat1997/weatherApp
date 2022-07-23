872dc3e6518e097079d43a2b4d9f4abba9ddfb6a
Prateek Singh <singhprateek21sept@gmail.com>
2022-07-23 23:56:36 +0530

    changing main.py

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def weather():
             city_name = request.form['location']
             city_name = city_name.upper()
             #print(city_name)
-            api_key = os.environ['weather_api']
+            #api_key = os.environ['weather_api']
+            api_key='6c98042f0a59d96abdc4dad334e810f2'
         
             #print(os.environ['weather_api'])
             url= "http://api.openweathermap.org/data/2.5/weather?q="
